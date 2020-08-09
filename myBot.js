a929383f2f77c22164e32b442969a3707743ca42
Michael Shuff <mjshuff23@gmail.com>
2020-08-08 20:21:34 -0400

    Updated comments in main file

--- myBot.js
+++ myBot.js
@@ -1,60 +1,60 @@
 const fs = require('fs');   // Import File System module
 const Discord = require('discord.js');    // Import discord.js12
 const { prefix, token }  = require('./config.json'); // destructure elements from config
-
-const client = new Discord.Client();      // Create new client
+// Initialize new clients and collections
+const client = new Discord.Client();
 client.commands = new Discord.Collection();
-
+const cooldowns = new Discord.Collection();
+// Read commands directory and add js files to variable
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
+// Import each command file and set them as well
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
 }
-
-const cooldowns = new Discord.Collection();
-
+// Happens once at login
 client.once('ready', () => {
     console.log("Connected as " + client.user.username);
     const homeChannel = client.channels.cache.get("733491269216763969");
     // homeChannel.send(`Kon'nichiwa sekai! I am ${client.user.username}!`);
 });
-
+// Happens for every message happening in the server
 client.on('message', message => {
     // Logs color chat
     client.commands.get('colorChat').execute(message);
     if (!message.content.startsWith(prefix) || message.author.bot) return;
-
+    // Separate command name and arguments to pass it
     const args = message.content.slice(prefix.length).trim().split(/ +/);
     const commandName = args.shift().toLowerCase();
-
+    // Report back when a command doesn't exist
     if (!client.commands.has(commandName)) {
         message.reply(`Oro, I don't think ${commandName} is something I can do!`);
         return;
     }
-
+    // Grab command out of commands Collection
     const command = client.commands.get(commandName);
-
+    // Check if command is server only
     if (command.guildOnly && message.channel.type !== 'text') {
         return message.reply(`I can't execute that command inside DMs!`);
     }
-
+    // Check if correct arguments are provided
     if (command.args && !args.length) {
         let reply = `You didn't provide the correct arguments, ${message.author}!`;
-
+        // Provide correct usage to user
         if (command.usage) {
             reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
         }
         return message.channel.send(reply);
     }
-
+    // Check if the command has cooldown set, if not, set one
     if (!cooldowns.has(command.name)) {
         cooldowns.set(command.name, new Discord.Collection());
     }
+    // Set variables to measure cooldown, set cooldown to 3s if not provided
     const now = Date.now();
     const timestamps = cooldowns.get(command.name);
     const cooldownAmount = (command.cooldown || 3) * 1000;
-
+    // Check if user has a cooldown on that command
     if (timestamps.has(message.author.id)) {
             const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
 
@@ -63,12 +63,12 @@ client.on('message', message => {
                 return message.reply(`Please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \'${command.name}\' command.`);
             }
     }
-
+    // Set a timer to delete the cooldown timestamp when time is us
     timestamps.set(message.author.id, now);
     setTimeout(() => {
         timestamps.delete(message.author.id);
     }, cooldownAmount);
-
+    // Execution time baby
     try {
 	    command.execute(message, args, command, client);
     } catch (error) {
